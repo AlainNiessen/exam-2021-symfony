e746c439cf280f0c6ab116e3a9b5683c988d11fc
AlainNiessen <60482327+AlainNiessen@users.noreply.github.com>
2021-11-30 19:34:05 +0100

    Test

--- src/Controller/LivreController.php
+++ src/Controller/LivreController.php
@@ -4,17 +4,50 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Livre;
+use App\Form\LivreType;
+use DateTime;
 
 class LivreController extends AbstractController
 {
     /**
-     * @Route("/livre", name="livre")
+     * @Route("/livre/form/creation", name="livreFormCreation")
      */
-    public function index(): Response
+    public function formCreationLivre(Request $request, EntityManagerInterface $manager): Response
     {
-        return $this->render('livre/index.html.twig', [
-            'controller_name' => 'LivreController',
+        //création nouveau objet Livre
+        $livre = new Livre();
+        $livre -> setDateAjout(new DateTime());
+        $livre -> setVotes(0);
+        
+
+        //création formulaire de création sur base de l'objet GenreType
+        $formCreationLivre = $this -> createForm(LivreType::class, $livre);
+        $formCreationLivre -> handleRequest($request);
+
+        //si le fomulaire est submit et validé => récupération des données rentrées et insertion dans la base de données
+        if($formCreationLivre -> isSubmitted() && $formCreationLivre -> isValid()) {
+            //récupération de données rentrées dans le formulaire
+            $livre = $formCreationLivre -> getData();
+
+            //préparation insertion genre dans la base de données via entitymanager:::::
+            $manager -> persist($livre);
+            //execution insertion
+            $manager -> flush();
+
+            //ajout d'un message de réussite d'insertion (affiché dans base.html.twig, alors elle sera affiché dans n'importe quel
+            //cas de redirection de route)
+            $this->addFlash('success', 'Livre créé avec succés');
+
+            //redirect
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('livre/formCreation.html.twig', [
+            'formCreationLivre' => $formCreationLivre -> createView()
         ]);
     }
 }
---
+++ src/Form/LivreType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Livre;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class LivreType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre', TextType::class, ['help' => 'Veuillez introduire un titre pour le livre'])
+            ->add('resume', TextAreaType::class, ['help' => 'Veuillez introduire un résumé pour le livre'])
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Livre::class,
+        ]);
+    }
+}
